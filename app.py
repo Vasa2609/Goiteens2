805756a6021a81c20b9553594c6c895e3c2feabc
Stefurak Vasyl <vasilstifurak303@gmail.com>
2023-11-26 20:49:15 +0200

    do base html

--- app.py
+++ app.py
@@ -1,50 +1,100 @@
 import os
-from flask import Flask, render_template, request, send_file
+from datetime import datetime
+from flask import Flask, render_template, request, redirect
 from dotenv import load_dotenv
-from enums import MAX_SCORE, NAME, student
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 
 app = Flask(__name__)
 
+db = SQLAlchemy() # db intitialized here
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db.init_app(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Article {self.id}>'
+
 
 @app.route("/")
-def main():
-    return render_template("index.html")
-
-@app.route("/base")
 def base():
-    return render_template("base.html", title="Python course for Goiteens")
+    return render_template("base.html", title="Python course")
 
 
-@app.route("/birthday")
-def birthday():
-    return render_template("birthday.html", title="2010")
-
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login_user():
+@app.route("/create-article", methods=["POST", "GET"])
+def create_article():
     if request.method == "POST":
-        user = request.form.get("name")
-        return "Method POST"
+        title = request.form["title"]
+        intro = request.form["intro"]
+        text = request.form["text"]
+
+        article = Article(
+            title=title,
+            intro=intro,
+            text=text
+        )
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect("/articles")
+        except Exception as exc:
+            return f"ПРи збереженні запису у базу даних виникла помилка: {exc}"
     else:
-        user = request.args.get("name")
-        return f"Method GET, user is {user}"
+        return render_template("create_article.html")
 
 
+@app.route("/articles")
+def list_articles():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template("articles.html", articles=articles)
 
-@app.route("/context")
-def context():
-    context_dict = {
-        "title": "Python_course",
-        "name": NAME,
-        "max_score": MAX_SCORE,
-        "student": student
-    }
-    return render_template("context.html", **context_dict)
+
+@app.route("/articles/<int:id>/")
+def article_detail(id):
+    article = Article.query.get(id)
+    return render_template("article_detail.html", article=article)
+
+
+@app.route("/articles/<int:id>/delete")
+def article_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect("/articles")
+    except Exception as exc:
+        return f"При видаленні виникла помилка: {exc}"
+
+
+@app.route("/articles/<int:id>/update", methods=["POST", "GET"])
+def article_update(id):
+    article = Article.query.get(id)
+
+    if request.method == "POST":
+        article.title = request.form["title"]
+        article.intro = request.form["intro"]
+        article.text = request.form["text"]
+
+        try:
+            db.session.commit()
+            return redirect("/articles")
+        except Exception as exc:
+            return f"При оновленні запису виникла помилка: {exc}"
+    else:
+        return render_template("article_update.html", article=article)
+
+
 
 
 if __name__ == "__main__":
     app.run(debug=os.getenv("DEBUG"))
-
