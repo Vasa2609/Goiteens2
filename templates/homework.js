27b3b21429faa8045b569b99a776a4318ff7dde3
Stefurak Vasyl <vasilstifurak303@gmail.com>
2024-01-03 13:59:22 +0200

    дз за 16.11

--- templates/homework.js
+++ templates/homework.js
@@ -1,7 +1,14 @@
+//window.onload = function () {
+//    let onload_window_zmina = document.getElementById("zmina");
+//    let age = prompt("Скільки тобі років");
+//    let years = age * 2;
+//    let result = "Число: " + years;
+//    onload_window_zmina.innerHTML = result;
+
+
 window.onload = function () {
     let onload_window_zmina = document.getElementById("zmina");
-    let username = prompt("введи ім'я");
-    let password = username * 3;
-    let result = "Число: " * password;
-    onload_window_zmina.innerHTML = result;
+    let username = prompt("введи ім'я")
+    let password = prompt("введи пароль")
+    onload_window_zmina.innerHTML = "<h1>Hello world!!!!</h1>"
 }
